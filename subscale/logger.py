e1b8335f228e981a7351e4f35d6f27d81ce6515c
yujitakai <yuji21eagles0912@yahoo.co.jp>
2019-11-17 05:36:48 -0500

    logger with serial connection code added

--- subscale/logger.py
+++ subscale/logger.py
@@ -1,6 +1,9 @@
 from sense_hat import SenseHat
 from datetime import datetime
+import time
 import os
+import RPi.GPIO as GPIO
+import serial
 
 class SensorData():
     def __init__(self, timestamp, accel, gyro, temp, p, humidity):
@@ -11,6 +14,7 @@ class SensorData():
         self.p = p
         self.humidity = humidity
 
+
 class Logger():
     def __init__(self):
         self.log_directory = "/home/pi/sensehat_test/data"
@@ -18,6 +22,11 @@ class Logger():
         self.sense = SenseHat()
         today = datetime.today()
         self.log_fname = "{}_{}_{}_{}:{}.csv".format(today.year, today.month, today.day, today.hour, today.minute)
+        self.port = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
+        self.start = None
+        self.launched = False
+        self.activated = False
+        self.locked = False
 
     def log(self):
         try:
@@ -38,13 +47,40 @@ class Logger():
         blue = (0, 0, 255)
         self.sense.show_message("OK", text_colour=green)
         while True:
-            sense_data.append(SensorData(datetime.now(), self.sense.get_accelerometer_raw(),
-                self.sense.get_orientation(), self.sense.get_temperature(), self.sense.get_pressure(),
+            acceler = self.sense.get_accelerometer_raw()
+            sense_data.append(SensorData(datetime.now(), acceler,
+                self.sense.get_orientation(), self.sense.get_temperature(),
+                self.sense.get_pressure(),
                 self.sense.get_humidity()))
             counter += 1
             if counter % 10 == 0:
                 self.write_csv(sense_data)
                 sense_data = []
+            if counter % 500 == 0:
+                self.port.write("GOOD")
+
+            # If the acceleration is more than 5G (less -5G b/c of the
+            # orientation), determine that the rocket was launched
+            if not self.launched and acceler['x'] <= -5:
+                self.launched = True
+
+            # If the rocket was launched and the acceleration is negative G
+            # and there is no activation made yet, activate the mechanism
+            # activated is turned on
+            if self.launched and acceler['x'] > 0 and not self.activated and not self.locked:
+                self.activated = True
+                self.start = time.time()
+                self.activate()
+
+            if self.activated and counter % 10 == 0:
+                self.activate()
+
+            if activated and time.time() - self.start >= 60:
+                # set locked true to no longer call activated (stops sending command to Arduino)
+                self.locked = True
+                # set activated false to not call activate anymore once in 10 cycles
+                self.activated = False
+
             if self.shutdown_armed:
                 for event in self.sense.stick.get_events():
                     print("The joystick was {} {}".format(event.action, event.direction))
@@ -62,6 +98,11 @@ class Logger():
                     data.accel['x'], data.accel['y'], data.accel['z'],
                     data.gyro['pitch'], data.gyro['roll'], data.gyro['yaw'],
                     data.temp, data.p, data.humidity))
+
+    def activate(self):
+        self.port.write("MOVE")
+
+
 
 def main():
     """
